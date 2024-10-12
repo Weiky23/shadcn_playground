4ec31a8eddd600ca078c4e17366321f63971aedf
Mikhail <23mikha@gmail.com>
2024-10-12 15:11:57 +0300

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 // import { useState } from 'react'
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-      <h1 className="text-9xl font-bold underline">
-          Hello world!
-      </h1>
+      <>
+      {/*<h1 className="text-4xl font-bold underline">*/}
+      {/*    Hello world!*/}
+      {/*</h1>*/}
+          <Button
+              variant="destructive">Click me
+          </Button>
+      </>
   )
 }
 
