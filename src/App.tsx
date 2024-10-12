570bc22ac0865f696ae26c37e6ccae341357ce09
Mikhail <23mikha@gmail.com>
2024-10-12 15:19:11 +0300

    Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
 import {Button} from "@/components/ui/button.tsx";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -10,9 +21,29 @@ function App() {
       {/*<h1 className="text-4xl font-bold underline">*/}
       {/*    Hello world!*/}
       {/*</h1>*/}
-          <Button
-              variant="destructive">Click me
-          </Button>
+      {/*    <Button*/}
+      {/*        variant="default">Click me*/}
+      {/*    </Button>*/}
+
+          <Drawer>
+              <DrawerTrigger asChild>
+                  <Button
+                      variant="default">Open Drawer
+                  </Button>
+              </DrawerTrigger>
+              <DrawerContent>
+                  <DrawerHeader>
+                      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                  </DrawerHeader>
+                  <DrawerFooter>
+                      <Button>Submit</Button>
+                      <DrawerClose asChild>
+                          <Button variant="outline">Cancel</Button>
+                      </DrawerClose>
+                  </DrawerFooter>
+              </DrawerContent>
+          </Drawer>
       </>
   )
 }
